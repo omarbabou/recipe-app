16c18cc0e3dd7ee8cdc85b6a58efc62aae3fb0e0
omarbabou <obabou50@gmail.com>
2022-12-21 16:24:29 +0200

    Update Views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:index, :show] do
+  end
+  root "users#index"
 end
