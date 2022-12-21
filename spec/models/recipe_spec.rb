ad4b8550d4cbada652bbcec99286e34e7ee5217f
omarbabou <obabou50@gmail.com>
2022-12-21 18:52:21 +0200

    Include tests files to the project

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+class RecipeTest < ActiveSupport::TestCase
+    # test "the truth" do
+    #   assert true
+    # end
 end
