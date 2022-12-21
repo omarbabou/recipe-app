843f5f9d7e370f85849506ea8ca51ad344fbe98a
omarbabou <obabou50@gmail.com>
2022-12-21 16:56:36 +0200

    Generate spec files for tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -43,7 +43,7 @@ RSpec.describe "/users", type: :request do
       
         describe "GET /new" do
           it "renders a successful response" do
-            get new_user_url
+            get users_url
             expect(response).to be_successful
           end
         end
