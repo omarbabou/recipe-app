e19adddffb220454b0a8a1387f4529ba683a4f1a
benardop <benard.oc.p@gmail.com>
2022-12-23 17:38:32 +0300

    Update routes files

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
   get 'shopping_list/index'
   get 'shopping_list/show'
-  resources :recipe_foods
-  devise_for :user
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :recipes
-  resources :foods, only: [:index, :show, :new, :create, :destroy]
-  resources :food_recipes
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :users, only: [:index, :show] do
-  resources :shopping_list, only: [:index ,:show]
+  devise_for :users
+  get 'recipes/index', to: 'recipes#public'
+  resources :users do
+    resources :recipes do
+    resources :recipe_foods, only: [:new, :create, :destroy, :update, :edit]
+    resources :shopping_list, only: [:index ,:show]
+    end
   end
-  root "users#index"
+  resources :users do
+    resources :foods
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "recipes#public"
 end
