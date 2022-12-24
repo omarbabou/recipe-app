ae4253f1433087dab3e8689fedd985c4a5cb1a91
omarbabou <obabou50@gmail.com>
2022-12-24 11:21:45 +0200

    Fix linters error

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,12 +1,13 @@
-# frozen_string_literal: true
-
 class Ability
+
   include CanCan::Ability
 
   def initialize(user)
     cannot :read, :all
-    can :create, :all
-    can :destroy, :all, user_id: user_id 
     return unless user.present?
+
+    can :create, :all
+    can :destroy, :all, user_id:
   end
 end
+
