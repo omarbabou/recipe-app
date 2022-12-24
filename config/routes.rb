3ffab09612ee768e8a244ece9855102ca3242bed
omarbabou <obabou50@gmail.com>
2022-12-24 09:37:06 +0200

    Add db credentials to the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,13 @@
 Rails.application.routes.draw do
-  get 'shopping_list/index'
-  get 'shopping_list/show'
-  devise_for :users
-  get 'recipes/index', to: 'recipes#public'
-  resources :users do
-    resources :recipes do
-    resources :recipe_foods, only: [:new, :create, :destroy, :update, :edit]
-    resources :shopping_list, only: [:index ,:show]
-    end
-  end
-  resources :users do
-    resources :foods
-  end
+  resources :recipe_foods
+  devise_for :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :recipes
+  resources :foods, only: [:index, :show, :new, :create, :destroy]
+  resources :food_recipes
   # Defines the root path route ("/")
-  root "recipes#public"
+  # root "articles#index"
+  resources :users, only: [:index, :show] do
+  end
+  root "users#index"
 end
